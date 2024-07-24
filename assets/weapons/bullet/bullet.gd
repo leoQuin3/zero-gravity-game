89f0f99aa668ae211a6d419cf9137665310df3a0
leoQuin3 <nanoat03@gmail.com>
2024-07-24 15:32:34 -0700

    Added ATDO enemy tscn.

--- assets/weapons/bullet/bullet.gd
+++ assets/weapons/bullet/bullet.gd
@@ -11,3 +11,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += initialVelocity * delta
+
+func _on_body_entered(body):
+	queue_free()
