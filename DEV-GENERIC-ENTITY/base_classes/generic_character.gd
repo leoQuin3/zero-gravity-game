4c8837d77513fcd95f92a9cf063dd4253ce6b156
leoQuin3 <nanoat03@gmail.com>
2025-10-30 00:26:02 -0700

    Created base classes for Generic Entities and Components

---
+++ DEV-GENERIC-ENTITY/base_classes/generic_character.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name GenericCharacter
+
+# ------- Attributes -------
+var speed: float
+var jump_velocity: float
+var look_direction: Vector3
+
+# ------ Methods ------
+func move() -> void:
+	pass
+
+func look() -> void:
+	pass
+
+func jump() -> void:
+	pass
+
+func die() -> void:
+	pass
---
+++ DEV-GENERIC-ENTITY/base_classes/generic_component.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name GenericComponent
+
+# Signals, variables, and methods go here!
+
+# -------------------------- Signals --------------------------
+# signal onEventA
+# signal onEventB(dataBeingPassed: type, otherData: type, ...)
+# ...
+
+# ----------- Attributes -----------
+# var attribute: type = value
+# var otherAttribute: type = value
+# ...
+
+# ----- Methods -----
+# func _ready():
+# 	...
+# func doSomething():
+# 	...
+# func sendASignal():
+#	...
+# ...
