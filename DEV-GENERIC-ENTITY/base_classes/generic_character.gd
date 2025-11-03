a3333dd256a25840c235764258b9b33be3985a63
leoQuin3 <nanoat03@gmail.com>
2025-11-03 01:45:30 -0800

    Refined Generic Character and Controller classes. Can now control Character.

--- DEV-GENERIC-ENTITY/base_classes/generic_character.gd
+++ DEV-GENERIC-ENTITY/base_classes/generic_character.gd
@@ -1,20 +1,24 @@
 extends CharacterBody3D
 class_name GenericCharacter
 
+# ---------------------------------- Signals ----------------------------------
+signal connected_to_controller
+signal disconnected_from_controller
+
 # -------------------------------- Attributes ---------------------------------
+var move_friction: float = 0.35
 var speed: float = 200
-var jump_velocity: float = 200
+var jump_velocity: float = 300
 var is_controlled: bool = false
 
 # ---------------------------------- Methods ----------------------------------
-#FIXME: Player drifts when disconnected from controller
 func move(direction: Vector3, delta) -> void:
 	if direction:
-		velocity.x = move_toward(velocity.x, direction.x * speed * delta, 0.5)
-		velocity.z = move_toward(velocity.z, direction.z * speed * delta, 0.5)
-	else:
-		velocity.x = move_toward(velocity.x, 0, 0.5)
-		velocity.z = move_toward(velocity.z, 0, 0.5)
+		velocity.x = move_toward(velocity.x, direction.x * speed * delta, move_friction)
+		velocity.z = move_toward(velocity.z, direction.z * speed * delta, move_friction)
+	elif direction == Vector3.ZERO:
+		velocity.x = move_toward(velocity.x, 0, move_friction)
+		velocity.z = move_toward(velocity.z, 0, move_friction)
 	
 func jump(delta) -> void:
 	if is_on_floor():
@@ -24,8 +28,28 @@ func jump(delta) -> void:
 func die() -> void:
 	pass
 
-func _process(delta):
+# ----------------------------- Reciever Methods -----------------------------
+func _on_connected_to_controller():
+	print("controlling")
+	is_controlled = true
+
+func _on_disconnected_from_controller():
+	print("disconnecting")
+	is_controlled = false
+	
+# --------------------------------- Main Loop ---------------------------------
+# Connect internal receiver methods before controller is ready
+func _enter_tree():
+	connected_to_controller.connect(Callable(self, "_on_connected_to_controller"))
+	disconnected_from_controller.connect(Callable(self, "_on_disconnected_from_controller"))
+
+func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y -= 9.8 * delta
+	
+	# Remain idle if not controlled
+	print(is_controlled)
+	if !is_controlled:
+		move(Vector3.ZERO, delta)
 	
 	move_and_slide()
--- DEV-GENERIC-ENTITY/base_classes/generic_controller.gd
+++ DEV-GENERIC-ENTITY/base_classes/generic_controller.gd
@@ -1,61 +1,43 @@
 extends Node
 class_name GenericController
 
-# Signals
-signal control_moved(direction: Vector3)
-signal control_looked(direction: Vector3)
-signal control_jumped()
-
 # -------------------------------- Attributes --------------------------------
 # A character to be controlled
 @export var puppet: GenericCharacter
 
 # ------------------------------- Class Methods -------------------------------
-# Make character puppet
+# Add character to controller
 func assign_puppet(character: GenericCharacter) -> void:
-	puppet = character
-	connect_puppet()
+	if !puppet:
+		puppet = character
+		puppet.connected_to_controller.emit()
 
-# Disconnect character as puppet
+# Remove character from controller
 func remove_puppet() -> void:
-	puppet = null
-	disconnect_puppet()
-
-# Connect controller signals to puppet's methods
-func connect_puppet():
-	control_moved.connect(Callable(puppet, "move"))
-	control_looked.connect(Callable(puppet, "look"))
-	control_jumped.connect(Callable(puppet, "jump"))
-
-# Disconnect controller signals to puppet's methods
-func disconnect_puppet():
-	control_moved.disconnect(Callable(puppet, "move"))
-	control_looked.disconnect(Callable(puppet, "look"))
-	control_jumped.disconnect(Callable(puppet, "jump"))
+	if puppet:
+		puppet.disconnected_from_controller.emit()
+		puppet = null
 
 # --------------------------- Puppet Method Callers ---------------------------
 # Call puppet's move() method
 func control_move(delta) -> void:
-	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	var move_dir = Vector3(input_dir.x, 0, input_dir.y)
-	emit_signal("control_moved", move_dir, delta)
+	if puppet:
+		var input_dir = Input.get_vector("left", "right", "forward", "back")
+		var move_dir = Vector3(input_dir.x, 0, input_dir.y)
+		puppet.move(move_dir, delta)
 
 # Call puppet's look() method
 func control_jump(delta) -> void:
-	if Input.is_action_just_pressed("jump"):
-		emit_signal("control_jumped", delta)
-
-# ----------------------------- Reciever Methods -----------------------------
-#TEMP: testing what happens when puppet is disconnected
-func _on_timer_timeout():
-	print("Disconnecting player!!!")
-	disconnect_puppet()
+	if puppet and Input.is_action_just_pressed("jump"):
+		puppet.jump(delta)
 
 # --------------------------------- Main Loop ---------------------------------
 func _ready():
 	if puppet:
-		connect_puppet()
+		puppet.connected_to_controller.emit()
 
 func _process(delta):
 	control_move(delta)
 	control_jump(delta)
+
+# ----------------------------- Reciever Methods -----------------------------
